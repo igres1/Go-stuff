d9f9d6c083e31469881bcd5a3f37b9d50813ee3d
igres1 <sergi.mndez@gmail.com>
2025-07-17 16:39:12 +0200

    added funcions to the Profit_Calculator with string format

--- Profit_Calculator_GO/calc.go
+++ Profit_Calculator_GO/calc.go
@@ -4,21 +4,32 @@ import "fmt"
 
 func main() {
 
-	var revenue float32
-	var expenses float32
-	var taxes float32
+	var revenue float64
+	var expenses float64
+	var taxes float64
 
-	fmt.Print("Enter Revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter Expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter  taxes to pay in decimal: ")
-	fmt.Scan(&taxes)
+	revenue = readUserInput("revenue: ")
 
+	expenses = readUserInput("Enter Expenses: ")
+
+	taxes = readUserInput("Enter taxes to pay: ")
+
+	EBT, profit, taxes := calculate(revenue, expenses, taxes)
+
+	fmt.Printf("EBT = %3f\n", EBT)
+	fmt.Printf("Profit = %3f\n", profit)
+	fmt.Printf("Taxes= %3f", taxes)
+}
+func readUserInput(info string) float64 {
+	var Input float64
+	fmt.Print(info)
+	fmt.Scan(&Input)
+	return Input
+
+}
+func calculate(revenue, expenses, taxes float64) (float64, float64, float64) {
 	EBT := revenue - expenses
 	profit := EBT - (EBT * taxes / 100)
 	ratio := EBT / profit
-
-	fmt.Print(ratio)
-
+	return EBT, profit, ratio
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+#Go stuff
+This repository is a collection of small project , experiments and random code while I'm learning go
+
+## About
+
+-Im using this space to practice the new concepts that i will be learning during the course im doing.
+-Also to try out extra things to improve the stuff done in the course
+
+## Disclaimer 
+
+This is a learning repo therefore some code my be messy , incomplete , or with short explanation .
